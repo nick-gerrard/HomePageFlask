ef8f16132a736f5bc48888e0fd0854625cf068d6
nick-gerrard <nick.gerrard16@gmail.com>
2020-05-15 11:09:34 -0400

    weather from openweatherapi | updated links to store in JSON form

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ import lxml
 from bs4 import BeautifulSoup
 import time
 import json
+import openweather
+
 
 news_url = "https://www.nytimes.com/sitemaps/new/news.xml.gz"
-weather_url = "https://patch.com/virginia/arlington-va/weather"
-links = {"Gmail": "https://www.mail.google.com",
-    "F A C E B O O K":"https://www.facebook.com/",
-    "YouTube": "https://www.youtube.com",
-    "Netflix": "https://www.netflix.com",
-    "Hulu" : "https://www.hulu.com"
-}
+weather_url = "https://www.wunderground.com/hourly/us/va/arlington/22204"
+
+def get_weather():
+    return openweather.get_arlington_weather(openweather.full_arlington_url)
 
 def get_links(file):
     f = open(file)
@@ -35,18 +34,9 @@ def greeting(name):
         greet = "Good Evening, " + name
         return greet
 
-def get_temp(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
-    try:
-        div = soup.find("div", {"class": "weather-current-temp temperature above-48 inline-block"})
-        current_temp = div.text
-    except:
-        div = soup.find("div", {"class": "weather-current-temp temperature above-62 inline-block"})
-        current_temp = div.text
-    # There's room in here to find quite a bit more data, like conditions, future temps, etc.
-    return current_temp
 
+
+    
 def get_trending(news_url):
     trending = []
     r = requests.get(news_url)
@@ -71,10 +61,9 @@ def nick():
     open_time = adjusted_time.strftime("%Y.%m.%d|%H:%M:%S")
     trending_stories = get_trending(news_url)
     greeting_type = greeting(name)
-    temp = get_temp(weather_url)
-    return render_template('nick.html', temp=temp, links=links, greeting=greeting_type, 
-                            weather_url=weather_url, trending_stories=trending_stories,
-                            open_time=open_time)
+    weatherdata = get_weather()
+    return render_template('nick.html', weatherdata=weatherdata, links=links, greeting=greeting_type, 
+                            trending_stories=trending_stories, open_time=open_time, weather_url = weather_url)
 
 @app.route('/time_feed')
 def time_feed():
