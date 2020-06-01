6f7deb27774c6efd46ad417b606e47bf6ecf4ff8
Nick Gerrard <nick.gerrard16@gmail.com>
2020-06-01 13:10:03 -0400

    updated database

--- homepage/formsDEPRECATED.py
+++
@@ -1,8 +0,0 @@
-"""
-import requests, os, json
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from homepage.models import User, Link, Message
-from homepage import db
-"""
