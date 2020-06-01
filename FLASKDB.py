e7aabfb41678e5def4fa92179fea044a0ee3a196
Nick Gerrard <nick.gerrard16@gmail.com>
2020-06-01 14:05:41 -0400

    updated db script with a change password function

--- FLASKDB.py
+++ FLASKDB.py
@@ -1,7 +1,18 @@
 #!/home/nickg/homepageflask/venv/bin/python
 
-from homepage import db, create_app
+from homepage import db, create_app, bcrypt
 from homepage.models import *
+
+
+def change_user_password(user, password):
+    check = input(f"Are you sure you want to change {user.username}'s password to {password}?")
+    if check.upper = "YES":
+        hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+        user.password = hashed_pw
+        db.session.commit()
+        print(f"Success! {user.username}'s password has been updated")
+    else:
+        print("Aborting password change")
 
 # Pushing the context to be able to query the database:\
 app = create_app()
